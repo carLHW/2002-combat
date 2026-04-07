61d82b5d6134f4d6fdbc34d0380984475590d754
koalahope <270854650+koalahope@users.noreply.github.com>
2026-04-07 18:32:31 +0800

    implemented wizard

--- src/combatants/Wizard.java
+++ src/combatants/Wizard.java
@@ -1,11 +1,27 @@
 package combatants;
 
+import actions.ArcaneBlastAction;
+import actions.BasicAttackAction;
+import actions.DefendAction;
+import actions.UseItemAction;
+import api.Combatant;
+import api.Item;
 import model.AbstractPlayer;
 
 public final class Wizard extends AbstractPlayer {
     public Wizard(String name) {
         super(name, 0, 0, 0, 0);
-        // TODO: replace placeholder stats with Wizard stats
-        // TODO: add Wizard actions
+    }
+    public Action UseBasicAttack(Combatant target){
+        return new BasicAttackAction(target);
+    }
+    public Action UseDefend(){
+        return new DefendAction();
+    }
+    public Action UseItem(Combatant target, Item item){
+        return new UseItemAction(target);
+    }
+    public Action UseArcaneBlast(Combatant target){
+        return new ArcaneBlastAction(target);
     }
 }
