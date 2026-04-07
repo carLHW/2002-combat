4be2dfed3a8b8ae93191cee6bbf789d49c784e38
koalahope <270854650+koalahope@users.noreply.github.com>
2026-04-07 18:29:43 +0800

    implemented warrior

--- src/combatants/Warrior.java
+++ src/combatants/Warrior.java
@@ -1,11 +1,29 @@
 package combatants;
 
+import model.AbstractEnemy;
 import model.AbstractPlayer;
+import actions.BasicAttackAction;
+import actions.DefendAction;
+import actions.ShieldBashAction;
+import actions.UseItemAction;
+import api.Action;
+import api.Combatant;
+import api.Item;
 
 public final class Warrior extends AbstractPlayer {
     public Warrior(String name) {
-        super(name, 0, 0, 0, 0);
-        // TODO: replace placeholder stats with Warrior stats
-        // TODO: add Warrior actions
+        super(name, 260, 40, 20, 30);
+    }
+    public Action UseBasicAttack(Combatant target){
+        return new BasicAttackAction(target);
+    }
+    public Action UseDefend(){
+        return new DefendAction();
+    }
+    public Action UseItem(Combatant target, Item item){
+        return new UseItemAction(target);
+    }
+    public Action UseShieldBash(Combatant target){
+        return new ShieldBashAction(target);
     }
 }
