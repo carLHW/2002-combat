e9b35f2df6aa00731844d0e6a2e51de2a3b6df44
Epillus <198755361+Epillus@users.noreply.github.com>
2026-04-08 13:39:49 +0800

    Update DefendStatusEffect.java

--- src/effects/DefendStatusEffect.java
+++ src/effects/DefendStatusEffect.java
@@ -5,9 +5,11 @@ import api.Combatant;
 import model.AbstractStatusEffect;
 
 public final class DefendStatusEffect extends AbstractStatusEffect {
+    private final int defenseBonus;
     public DefendStatusEffect(int defenseBonus) {
-        super(0);
-        // TODO: store defend bonus and duration
+        super(2);
+        // store defend bonus and duration
+        this.defenseBonus = defenseBonus;
     }
 
     @Override
@@ -17,6 +19,16 @@ public final class DefendStatusEffect extends AbstractStatusEffect {
 
     @Override
     public void onApply(Combatant target, BattleContext battleContext) {
-        // TODO: implement DefendStatusEffect
+        target.modifyDefense(defenseBonus);
+    }
+
+    @Override
+    public void onRoundEnd(Combatant target, BattleContext battleContext) {
+        reduceRoundsByOne();
+    }
+
+    @Override
+    public void onExpire(Combatant target, BattleContext battleContext) {
+        target.modifyDefense(-defenseBonus);
     }
 }
