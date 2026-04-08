e3b9910f9922807117c3f6a4a5c9558d1f21b1a7
dhtt <denghtcarrie@gmail.com>
2026-04-08 14:17:48 +0800

    Update PowerStoneItem.java

--- src/items/PowerStoneItem.java
+++ src/items/PowerStoneItem.java
@@ -1,11 +1,14 @@
 package items;
 
+import api.Action;
 import api.ActionTarget;
 import api.BattleView;
 import api.Item;
 import model.AbstractPlayer;
 
 public final class PowerStoneItem implements Item {
+
+    private static final String SPECIAL_SKILL_KEY = "SpecialSkill";
     @Override
     public String getName() {
         return "Power Stone";
@@ -13,12 +16,29 @@ public final class PowerStoneItem implements Item {
 
     @Override
     public boolean canUse(AbstractPlayer user, BattleView battleView) {
-        // TODO: decide whether there are any usage restrictions for Power Stone.
-        return true;
+        return user.getActions().stream()
+                .anyMatch(action -> SPECIAL_SKILL_KEY.equals(action.getName()));
     }
 
     @Override
     public void use(AbstractPlayer user, ActionTarget target) {
-        // TODO: implement PowerStoneItem
+        Action specialSkill = user.getActions().stream()
+                .filter(action -> SPECIAL_SKILL_KEY.equals(action.getName()))
+                .findFirst()
+                .orElse(null);
+
+        if (specialSkill == null) {
+            return;
+        }
+
+        int previousCooldown = user.getCooldownTracker().getRemainingTurns(SPECIAL_SKILL_KEY);
+
+        specialSkill.execute(user, target);
+
+        user.getCooldownTracker().startCooldown(SPECIAL_SKILL_KEY, previousCooldown);
+
+        if (target != null && target.context() != null) {
+            target.context().log(user.getName() + " activated Power Stone.");
+        }
     }
 }
