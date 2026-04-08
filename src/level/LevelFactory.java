e8bc0cedc80c5cf7e5e3e5e7badbc72f9c2cd85e
carLHW <110158116+carLHW@users.noreply.github.com>
2026-04-08 13:31:48 +0800

    .
    .

--- src/level/LevelFactory.java
+++ src/level/LevelFactory.java
@@ -1,13 +1,47 @@
 package level;
 
+import api.Combatant;
+import java.util.ArrayList;
 import java.util.List;
 
+// We assume these are in the model package based on your architecture
+import model.Goblin;
+import model.Wolf;
+
 public final class LevelFactory {
+    
     private LevelFactory() {
+        // Prevent instantiation of utility class
     }
 
     public static LevelSetup createLevel(Difficulty difficulty) {
-        // TODO: implement difficulty setup
-        return new LevelSetup(List.of(), List.of());
+        List<Combatant> initialEnemies = new ArrayList<>();
+        List<Combatant> backupEnemies = new ArrayList<>();
+
+        switch (difficulty) {
+            case EASY:
+                initialEnemies.add(new Goblin("Weak Goblin"));
+                // No backups for easy mode
+                break;
+                
+            case MEDIUM:
+                initialEnemies.add(new Goblin("Goblin Guard"));
+                initialEnemies.add(new Wolf("Wild Wolf"));
+                
+                // One backup enemy
+                backupEnemies.add(new Goblin("Sneaky Goblin")); 
+                break;
+                
+            case HARD:
+                initialEnemies.add(new Wolf("Alpha Wolf"));
+                initialEnemies.add(new Goblin("Goblin Chief"));
+                
+                // Two backup enemies
+                backupEnemies.add(new Wolf("Pack Wolf"));
+                backupEnemies.add(new Goblin("Goblin Shaman"));
+                break;
+        }
+
+        return new LevelSetup(initialEnemies, backupEnemies);
     }
 }
