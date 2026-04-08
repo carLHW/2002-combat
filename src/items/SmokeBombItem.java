1bba8a3295487ccb6e447d7dbd33ecaf56afba4b
dhtt <198755361+Epillus@users.noreply.github.com>
2026-04-08 14:22:43 +0800

    Update SmokeBombItem.java

--- src/items/SmokeBombItem.java
+++ src/items/SmokeBombItem.java
@@ -4,8 +4,13 @@ import api.ActionTarget;
 import api.BattleView;
 import api.Item;
 import model.AbstractPlayer;
+import api.BattleContext;
+import api.Combatant;
+import model.AbstractStatusEffect;
 
 public final class SmokeBombItem implements Item {
+
+    private static final int SMOKE_BOMB_DEFENSE_BONUS = 100;
     @Override
     public String getName() {
         return "Smoke Bomb";
@@ -13,12 +18,45 @@ public final class SmokeBombItem implements Item {
 
     @Override
     public boolean canUse(AbstractPlayer user, BattleView battleView) {
-        // TODO: decide whether there are any usage restrictions for Smoke Bomb.
         return true;
     }
 
     @Override
     public void use(AbstractPlayer user, ActionTarget target) {
-        // TODO: implement SmokeBombItem
+        BattleContext battleContext = target == null ? null : target.context();
+        user.addStatusEffect(new SmokeBombStatusEffect(SMOKE_BOMB_DEFENSE_BONUS), battleContext);
+
+        if (battleContext != null) {
+            battleContext.log(user.getName() + " used Smoke Bomb.");
+        }
+    }
+
+    private static final class SmokeBombStatusEffect extends AbstractStatusEffect {
+        private final int defenseBonus;
+
+        private SmokeBombStatusEffect(int defenseBonus) {
+            super(2);
+            this.defenseBonus = defenseBonus;
+        }
+
+        @Override
+        public String getName() {
+            return "Smoke Bomb";
+        }
+
+        @Override
+        public void onApply(Combatant target, BattleContext battleContext) {
+            target.modifyDefense(defenseBonus);
+        }
+
+        @Override
+        public void onRoundEnd(Combatant target, BattleContext battleContext) {
+            reduceRoundsByOne();
+        }
+
+        @Override
+        public void onExpire(Combatant target, BattleContext battleContext) {
+            target.modifyDefense(-defenseBonus);
+        }
     }
 }
