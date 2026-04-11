d76320d218651563d1aca79dd56a3ab659194ea4
Nacai <111849193+B67687@users.noreply.github.com>
2026-04-11 23:55:28 +0800

    Align combatants, items, levels, and engine with migrated structure

--- src/combatants/Wolf.java
+++ src/combatants/Wolf.java
@@ -1,18 +1,11 @@
 package combatants;
 
-import java.util.ArrayList;
-import java.util.List;
 import actions.BasicAttackAction;
-import api.Action;
 import model.AbstractEnemy;
 
 public final class Wolf extends AbstractEnemy {
     public Wolf(String name) {
         super(name, 40, 45, 5, 35);
-    }
-    public List<Action> getActions(){
-        List<Action> actions = new ArrayList<>();
-        actions.add(new BasicAttackAction());
-        return actions;
+        addAction(new BasicAttackAction());
     }
 }
