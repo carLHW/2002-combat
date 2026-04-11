ca2819c385e8278adc06c0bb2d9efda5082fa3ae
Nacai <111849193+B67687@users.noreply.github.com>
2026-04-11 23:53:21 +0800

    Migrate shared combat flow and player input structure

--- src/model/AbstractEnemy.java
+++ src/model/AbstractEnemy.java
@@ -1,13 +1,25 @@
 package model;
 
-import actions.BasicAttackAction;
+import api.Action;
+import api.ActionTarget;
+import api.BattleContext;
+import api.BattleView;
 import api.Team;
 
 // Shared base class for enemy combatants.
-// TODO: decide whether enemy-shared behavior is needed here beyond the constructor.
+// TODO: if enemy behavior becomes more advanced later, put the shared logic here.
 public abstract class AbstractEnemy extends AbstractCombatant {
     protected AbstractEnemy(String name, int maxHp, int attack, int defense, int speed) {
         super(name, Team.ENEMY, maxHp, attack, defense, speed);
-        addAction(new BasicAttackAction());
+    }
+
+    @Override
+    public Action chooseAction(BattleView battleView) {
+        return super.chooseAction(battleView);
+    }
+
+    @Override
+    public ActionTarget chooseTarget(Action action, BattleView battleView, BattleContext battleContext) {
+        return super.chooseTarget(action, battleView, battleContext);
     }
 }
